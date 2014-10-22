563031ada533e25082e3959fdebd2e4e6e6dfe94
Alex home <rombik_84@mail.ru>
2014-10-23 02:20:15 +0300

    Database PDO query, part 8/23

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 require_once('core/init.php');
 
-echo Config::get('mysql/host');
-DB::getInstance();
+$user = DB::getInstance()->get('users', array('username', '=', 'Alex473'));
+
+if(!$user->count()){
+    echo "Error!";
+}else{
+    echo "OK";
+}
